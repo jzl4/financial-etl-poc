40f4f5a9b6a7ac5aa2cff399814e3c91a5f9a39c
Ubuntu <ubuntu@ip-172-31-42-190.us-east-2.compute.internal>
2025-07-13 21:08:17 +0000

    Created DAG etl_ingest_tiingo_to_staging_dag, and need to clean it up a bit

--- airflow/dags/etl_ingest_tiingo_to_staging_dag.py
+++ airflow/dags/etl_ingest_tiingo_to_staging_dag.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 
 from datetime import datetime, timedelta
 
@@ -13,7 +14,8 @@ sys.path.append(project_root_folder)
 from etl_drivers.etl_ingest_tiingo_to_staging import main_airflow as ingest_tiingo_to_staging_main_airflow
 
 def log_failure(context):
-    
+    with open("/opt/airflow/logs/failure_alerts.log", "a") as f:
+        f.write(f"[{datetime.now()}] DAG {context["dag"].dag_id} failed on task {context["task_instance"].task_id}\n")
 
 default_args = {
     "owner": "joe_lu",
@@ -22,3 +24,20 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(seconds = 30)
 }
+
+with DAG(
+    dag_id = "etl_ingest_tiingo_to_staging_daily",
+    default_args = default_args,
+    description = "Extracts daily closing prices and volumes from Tiingo API and inserts into tbl_tiingo_daily_staging",
+    schedule_interval = "0 18 * * 1-5",
+    start_date = datetime(2025, 7, 14),
+    catchup = False,
+    tags = ["Tiingo", "ETL"]
+) as dag:
+    
+    run_etl_ingest_tiingo_to_staging = PythonOperator(
+        task_id = "run_etl_ingest_tiingo_to_staging",
+        python_callable = ingest_tiingo_to_staging_main_airflow
+    )
+
+    run_etl_ingest_tiingo_to_staging
